9729852f024c1e0aa43fe85a9dc902d7652099e8
amps93 <kmk23534202@gmail.com>
2024-09-24 19:14:58 +0900

    edit eeve code

--- chatbot_server/chat.py
+++ chatbot_server/chat.py
@@ -2,7 +2,6 @@ from langchain_community.llms import Ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-# 로컬환경에서 만들어놓은 EEVE 모델 사용
 llm_eeve = Ollama(model='eeve')
 
 # Prompt 설정
@@ -15,12 +14,9 @@ prompt = ChatPromptTemplate.from_messages(
         ),
 
         MessagesPlaceholder(variable_name="messages"),
-        # 최근 사용자와의 대화기록을 기억하기 위한 명령어
     ]
 )
 
-# LangChain 표현식
+# 체인 생성
 chain = prompt | llm_eeve | StrOutputParser()
-# prompt : LLM 설정
-# llm_eeve : LLM 종류
-# StrOutputParser() : 채팅 메시지를 문자열로 변환하는 간단한 출력 구문 분석기
+
--- chatbot_server/server.py
+++ chatbot_server/server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
-
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Union
 from pydantic import BaseModel, Field
@@ -12,8 +11,6 @@ from chat import chain
 app = FastAPI()
 
 
-# 추가된 코드
-# FastAPI 애플리케이션에서 Cross-Origin Resource Sharing(CORS) 설정을 관리하는 미들웨어
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # 모든 출처의 요청 허용
@@ -24,20 +21,15 @@ app.add_middleware(
 )
 
 
-# 수정된 코드
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/chat/playground")
 
 
-# 추가된 코드
-# 메시지 데이터를 효과적으로 모델링하고 유효성 검사와 데이터 관리 기능을 제공
 class Input(BaseModel):
-    """Input for the chat endpoint."""  # Input 클래스 용도
-
+    """Input for the chat endpoint."""
     messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = Field(
-        ...,
-    description = "the messages representing the current converation.", # Field 설명 추가
+        ..., description="the messages representing the current converation.",
     )
 
 
